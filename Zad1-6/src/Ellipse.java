c4c0b9671e59ef64dd01d8f5756875097d03787f
Амир <133702152+Mexanik-v-shortax@users.noreply.github.com>
2024-09-24 10:50:48 +0300

    Delete Zad1-6 directory

--- Zad1-6/src/Ellipse.java
+++
@@ -1,9 +0,0 @@
-public class Ellipse extends Practicum { // Ellipse наследуется от Circle
-    public void circleToEllipse() {
-        System.out.println("Превращаем круг в овал");
-    }
-
-    public void drawCircle() {
-
-    }
-}
