453c5bcc7631ae70ee40a4550a46242c0b032e09
Амир <133702152+Mexanik-v-shortax@users.noreply.github.com>
2024-09-24 10:51:11 +0300

    Add files via upload

---
+++ Zad1-6/src/Figure.java
@@ -0,0 +1,14 @@
+class Figure {
+public void drawCircle() {
+    System.out.println("Рисуем круг");
+}
+
+public void drawSquare() {
+    System.out.println("Рисуем квадрат");
+}
+
+public void drawRectangle() {
+    System.out.println("Рисуем прямоугольник");
+}
+}
+
