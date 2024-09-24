8abfeb940205695d4cff66373f64969929cbfb79
Амир <133702152+Mexanik-v-shortax@users.noreply.github.com>
2024-09-24 10:31:38 +0300

    Add files via upload

---
+++ Zad1-6/src/Ellipse.java
@@ -0,0 +1,9 @@
+public class Ellipse extends Practicum { // Ellipse наследуется от Circle
+    public void circleToEllipse() {
+        System.out.println("Превращаем круг в овал");
+    }
+
+    public void drawCircle() {
+
+    }
+}
